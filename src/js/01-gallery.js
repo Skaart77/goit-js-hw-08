526a291fb08e67af6d509e5f1860be0b8a6300a9
Serhii Skliarov <114434801+Skaart77@users.noreply.github.com>
2023-03-06 19:05:35 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const picturesContainer = document.querySelector('.gallery');
+
+// Формуємо розмітку галереї на основі масиву даних
+const createGalleryMarkup = galleryItems => {
+    return galleryItems.map(({ original, preview, description })=> {
+        return `
+       <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src=${preview} alt="${description}" />
+</a>`;
+    }).join('');
+};
+
+picturesContainer.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    overlayOpacity: 0.5,
+    captionDelay: 250,
+    captionsData: 'alt',
+});
