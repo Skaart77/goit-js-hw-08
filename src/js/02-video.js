6c1d58705acf16f1b8bbc62ab98f625927d2032a
Serhii Skliarov <114434801+Skaart77@users.noreply.github.com>
2023-03-11 17:53:20 +0200

    new

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+const storageTime = 'videoplayer-current-time';
+const TIME_DELAY = 1000;
+
+player.on('timeupdate', throttle(onPlayerTimeUpdate, TIME_DELAY));
+
+function onPlayerTimeUpdate(event) {
+    const time = Math.floor(event.seconds);
+    localStorage.setItem(storageTime, time);
+}
+const savedTime = localStorage.getItem(storageTime);
+
+if (savedTime) {
+  player.setCurrentTime(savedTime || 0);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = form.querySelector('input[name="email"]');
+const message = form.querySelector('textarea[name="message"]');
+const STOREGE_KEY = 'feedback-form-state';
+
+
+form.addEventListener('submit', submitForm);
+email.addEventListener('input', throttle(onTextareaInput, 500))
+message.addEventListener('input', throttle(onTextareaInput, 500))
+
+updateFormState();
+
+const state = {
+    email: email.value,
+    message: message.value
+  };
+
+function onTextareaInput() {    
+    state[email.name] = email.value;
+    state[message.name] = message.value;
+
+    localStorage.setItem(STOREGE_KEY, JSON.stringify(state))
+};
+
+function submitForm(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(STOREGE_KEY);
+    console.log(state);
+} 
+
+function updateFormState(){
+    const state = JSON.parse(localStorage.getItem(STOREGE_KEY));
+    if (state) {
+    email.value = state.email || '';
+    message.value = state.message || '';
+    }
+    
+
+}
